f276f73ca3b0449a76b6e47e8656fc649e96a9da
PEACE-DFG <awofesobipeace@gmail.com>
2023-05-14 23:58:17 +0100

    update ongoing

--- p&s.js
+++ p&s.js
@@ -106,4 +106,18 @@ fetch('https://dummyjson.com/products')
    
    
 
-})
+});
+let count_item= document.getElementById('counts')
+count_item.innerHTML=localStorage.length
+
+function addtocart(x){
+
+    let product_name= x.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+    let product_price=x.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+    let product_image=x.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.src
+    const all_item=JSON.stringify([product_name,product_image,product_price])
+    localStorage.setItem(product_name,all_item);
+
+    count_item.innerHTML=localStorage.length
+
+}
